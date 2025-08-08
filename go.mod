70666a711d67f00d0c803bd77fdd3a27b0179e0c
Rehan Daphedar <mail@rehandaphedar.com>
2025-08-08 19:44:49 +0530

    streamline api caching, new i/o format
    - Added ability to lafzize (multiple) verse ranges instead of just 1
    verse at a time
    - Changed the output format to QUL compatible format
    - Moved the api caching logic to a separate package. This package will
    be used in related projects

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
-module git.sr.ht/~rehandaphedar/lafzize/v2
+module git.sr.ht/~rehandaphedar/lafzize/v3
 
-go 1.23.1
+go 1.24.6
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	golang.org/x/oauth2 v0.30.0
+)
--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -8,14 +11,47 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 
+	"git.sr.ht/~rehandaphedar/lafzize/v3/pkg/api"
 	"github.com/google/uuid"
 )
 
-func runServer() {
-	port := os.Args[2]
-	address := fmt.Sprintf(":%s", port)
+type APIRequestSegment struct {
+	Start string `json:"start"`
+	End   string `json:"end"`
+}
+
+type APIResponse struct {
+	Segments [][]int `json:"segments"`
+}
+
+var data api.API
+var maxUploadSize *int
+var device *string
+
+func runServerCommand(args []string) {
+	serverFlagSet := flag.NewFlagSet("server", flag.ExitOnError)
+
+	port := serverFlagSet.Int("port", 8004, "Port to listen on")
+	maxUploadSize = serverFlagSet.Int("max_upload_size", 128, "Maximum size in MB of uploaded file")
+	dataPath := serverFlagSet.String("data", "data.json", "Path to file containing API data")
+	device = serverFlagSet.String("device", "cpu", "Device to use")
+
+	if err := serverFlagSet.Parse(args); err != nil {
+		serverFlagSet.Usage()
+		os.Exit(1)
+	}
+
+	dataFile, err := os.ReadFile(*dataPath)
+	if err != nil {
+		log.Fatalf("error reading file: %v\n", err)
+	}
+
+	if err := json.Unmarshal(dataFile, &data); err != nil {
+		log.Fatalf("error unmarshaling JSON: %v\n", err)
+	}
+
+	address := fmt.Sprintf(":%d", *port)
 
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(address, nil))
@@ -26,8 +62,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Parse multipart form data
-	if err := r.ParseMultipartForm(128 << 20); err != nil { // 128 MB max file size
+	if err := r.ParseMultipartForm(int64(*maxUploadSize) << 20); err != nil {
 		http.Error(w, fmt.Sprintf("Error parsing form data: %s", err), http.StatusBadRequest)
 		return
 	}
@@ -36,71 +71,103 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	requestDir := filepath.Join("data", "requests", requestUuid)
 	uploadedFilePath := filepath.Join(requestDir, "uploaded")
 	transcodedFilePath := filepath.Join(requestDir, "audio.wav")
-	wordTimestampsFilePath := filepath.Join(requestDir, "audio.json")
-	verseKey := r.FormValue("verse_key")
-	verseKeySafe := strings.ReplaceAll(verseKey, ":", "_")
-	verseTextFilepath := filepath.Join("data", "verse-text", fmt.Sprintf("%s.txt", verseKeySafe))
+	timingsFilePath := filepath.Join(requestDir, "audio.json")
+	wordsFilePath := filepath.Join(requestDir, "words.txt")
 
 	if err := os.MkdirAll(requestDir, 0755); err != nil {
 		http.Error(w, fmt.Sprintf("Error creating directory: %s", err), http.StatusInternalServerError)
 		return
 	}
 
-	// Get uploaded file
-	uploadedAudioFile, _, err := r.FormFile("file")
+	verseSegments, wordSegments := generateSegments(r.Form["segments"])
+
+	if err := writeWordsFile(wordsFilePath, verseSegments); err != nil {
+		http.Error(w, fmt.Sprintf("Error writing text file: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	uploadedFileMultipart, _, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error retrieving uploaded file: %s", err), http.StatusBadRequest)
 		return
 	}
-	defer uploadedAudioFile.Close()
+	defer uploadedFileMultipart.Close()
 
-	// Save uploaded file
-	if err := saveFile(uploadedFilePath, uploadedAudioFile); err != nil {
+	uploadedFile, err := os.Create(uploadedFilePath)
+	if err != nil {
 		http.Error(w, fmt.Sprintf("Error saving uploaded file: %s", err), http.StatusInternalServerError)
 		return
 	}
+	defer uploadedFile.Close()
 
-	// Transcode
+	if _, err := io.Copy(uploadedFile, uploadedFileMultipart); err != nil {
+		http.Error(w, fmt.Sprintf("Error copying uploaded file: %s", err), http.StatusInternalServerError)
+		return
+	}
+
 	if err := transcode(uploadedFilePath, transcodedFilePath); err != nil {
 		http.Error(w, fmt.Sprintf("Error processing uploaded file: %s", err), http.StatusInternalServerError)
 		return
 	}
 
-	// Generate timestamps
-	generateWordTimestamps(transcodedFilePath, verseTextFilepath)
+	if err := generateWordTimestamps(transcodedFilePath, wordsFilePath); err != nil {
+		http.Error(w, fmt.Sprintf("Error generating timestamps for uploaded file: %s", err), http.StatusInternalServerError)
+		return
+	}
 
-	// Return audio JSON
-	jsonData, err := readJSONData(wordTimestampsFilePath)
+	timingsBytes, err := os.ReadFile(timingsFilePath)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("Error reading JSON data: %s", err), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Error reading aligner output: %s", err), http.StatusInternalServerError)
+	}
+
+	var timings AlignerOutput
+	if err = json.Unmarshal(timingsBytes, &timings); err != nil {
+		http.Error(w, fmt.Sprintf("Error unmarshaling aligner output: %s", err), http.StatusInternalServerError)
+	}
+
+	var response APIResponse
+	response.Segments, err = convertAlignerOutput(timings, wordSegments)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error converting aligner output to response format: %s", err), http.StatusInternalServerError)
+		return
+	}
+
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error marshaling response to JSON: %s", err), http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(jsonData)
-	if err != nil {
+	if _, err = w.Write(responseJSON); err != nil {
 		http.Error(w, fmt.Sprintf("Error returning JSON data: %s", err), http.StatusInternalServerError)
 		return
 	}
 
-	err = os.RemoveAll(requestDir)
-	if err != nil {
-		log.Printf("Error while deleting directory: %v", err)
+	if err = os.RemoveAll(requestDir); err != nil {
+		log.Printf("Error while deleting directory: %v\n", err)
 	}
 }
 
-func saveFile(filePath string, file io.Reader) error {
-	localFile, err := os.Create(filePath)
+func writeWordsFile(textFilePath string, verseSegments []string) error {
+	textFile, err := os.Create(textFilePath)
 	if err != nil {
-		return fmt.Errorf("error creating file: %w", err)
+		return err
 	}
-	defer localFile.Close()
+	defer textFile.Close()
 
-	// Copy the file content to the destination
-	_, err = io.Copy(localFile, file)
-	if err != nil {
-		return fmt.Errorf("error writing file: %w", err)
+	writer := bufio.NewWriter(textFile)
+	defer writer.Flush()
+
+	for _, verseKey := range verseSegments {
+		words := data.Verses[verseKey].Words
+		for _, word := range words[:len(words)-1] {
+			if _, err := writer.WriteString(word.TextUthmani + "\n"); err != nil {
+				return err
+			}
+		}
 	}
+
 	return nil
 }
 
@@ -115,26 +182,14 @@ func transcode(inputPath, outputPath string) error {
 	return cmd.Run()
 }
 
-func generateWordTimestamps(audioPath string, textPath string) error {
-	device := os.Getenv("LAFZIZE_DEVICE")
-	if device == "" {
-		device = "cpu"
-	}
+func generateWordTimestamps(audioPath string, wordsFilePath string) error {
+	cmd := exec.Command("ctc-forced-aligner",
+		"--audio_path", audioPath,
+		"--text_path", wordsFilePath,
+		"--language", "\"ara\"",
+		"--romanize",
+		"--preserve_split", "True",
+		"--device", *device)
 
-	cmd := exec.Command("ctc-forced-aligner", "--audio_path", audioPath, "--text_path", textPath, "--language", "\"ara\"", "--romanize", "--preserve_split", "True", "--device", device)
-	_, err := cmd.Output()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func readJSONData(filePath string) ([]byte, error) {
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	return data, nil
+	return cmd.Run()
 }
